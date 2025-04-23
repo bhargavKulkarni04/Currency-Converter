20661e9f2155af2313389762c3190a83f68a1fcc
Bhargav S Kulkarni <bhargavkulkarni04@gmail.com>
2025-04-23 12:18:15 +0530

    changes made

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const Base_URL =
   "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json";
 const dropdown = document.querySelectorAll(".dropdown select");
+const button = document.querySelector("form button")
 
 //creating all the individual countries in CountryList into newOption and pusing into select
 for (let select of dropdown) {
@@ -17,11 +18,19 @@ for (let select of dropdown) {
     }
     select.append(newOption);
   }
-  select.addEventListener("change",(eve)=>{
-    updateFlag(eve.target)
-  })
+  select.addEventListener("change", (eve) => {
+    updateFlag(eve.target); //const updateFlag = (element)=>{ inside this i have eve.target so any changes in select appiles here
+  });
 }
 //As currCode changes and symultaneously change the country flag
-const updateFlag = ()=>{
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+button.addEventListener("click",(eve)=>{
+  eve.preventDefault()// let us keep our page default beofre submission
 
-}
+})
