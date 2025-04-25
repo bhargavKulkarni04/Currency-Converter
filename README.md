d1f674547f31f94ce1916133318f954c3ac77cc1
Bhargavkularni04 <bhargavkulkarni04@gmail.com>
2025-04-25 20:32:35 +0530

    all set to deploy

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Currency-Converter
 HTML,CSS,JS
 A currency converter is a web application that allows users to convert monetary values from one currency to another.  It provides a user-friendly interface with input for the amount, dropdowns for currency selection, and a display for the converted result.  The application fetches exchange rates from an API to ensure accurate conversions.
-access this website netlify
+access this website netlify and is LIVE
--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// const Base_URL = "https://latest.currency-api.pages.dev/v1/currencies";
-// const Base_URL = "https://api.frankfurter.app/latest";
-// const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies "
-// let BASE_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_7UStkUqQNBmahSoy8K635tE3Sjr5fK1UVPmVloZ2"
-// const Base_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{currencyCode}}";
-// const BASE_URL= "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/{endpoint}"
-const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1";
-
-
-
+const BASE_URL ="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1";
 const dropdown = document.querySelectorAll(".dropdown select");
 const button = document.querySelector("form button");
 const fromCurrency = document.querySelector(".from select");
 const toCurrency = document.querySelector(".to select");
-// const message = document.querySelector(".message")
 const message = document.querySelector(".message");
 
 //creating all the individual countries in CountryList into newOption and pusing into select
@@ -22,7 +12,7 @@ for (let select of dropdown) {
     newOption.innerText = currCode;
     newOption.value = currCode;
 
-    //to keep "from" - "USD" and "To" - "INR" 
+    //to keep "from" - "USD" and "To" - "INR"
     if (select.name === "from" && currCode === "USD") {
       newOption.selected = "selected";
     } else if (select.name === "to" && currCode === "INR") {
@@ -51,16 +41,13 @@ button.addEventListener("click", async (eve) => {
     amountValue = 1;
     amount.value = "1";
   }
-  // const URL = `${BASE_URL}/${fromCurrency.value.toLowerCase()}/${toCurrency.value.toLowerCase()}.json`
+
   const URL = `${BASE_URL}/currencies/${fromCurrency.value.toLowerCase()}.json`;
-  let resp = await fetch(URL);
-  let data = await resp.json()
-  // let rate =  data[toCurrency.value.toLowerCase()]
-  let rate = data[fromCurrency.value.toLowerCase()][toCurrency.value.toLowerCase()];
-  let FinalAmt = amountValue  * rate
-  message.innerText = `${amountValue} ${fromCurrency.value}=${FinalAmt} ${toCurrency.value}`
-  // message.innerText = `${amountValue} $ {fromCurrency.value} =${FinalAmt} ${toCurrency}`
- 
-  
-  
+  let resp = await fetch(URL); //sends an HTTPS req and waits for response
+  let data = await resp.json(); //converts raw response into JSON or javaScript formate
+
+  let rate =
+    data[fromCurrency.value.toLowerCase()][toCurrency.value.toLowerCase()];
+  let FinalAmt = amountValue * rate;
+  message.innerText = `${amountValue} ${fromCurrency.value}=${FinalAmt} ${toCurrency.value}`;
 });
